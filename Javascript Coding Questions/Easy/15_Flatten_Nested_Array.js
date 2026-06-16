6994416d392331ba0999accfb5535dad88d42c3b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-06-16 09:29:28 +0530

    why not push

--- Javascript Coding Questions/Easy/15_Flatten_Nested_Array.js
+++ Javascript Coding Questions/Easy/15_Flatten_Nested_Array.js
@@ -18,9 +18,10 @@ const flattenArrayTwo=(arr)=>{
     const result=[]
     for(let i=0;i<arr.length;i++){
         if(Array.isArray(arr[i])){
+            /*
+            Note: why .push() is not used in place of .concat(), explained below:
+            */
             result=result.concat(flattenArrayTwo(arr[i]))
-            //or
-            //result=result.push(flattenArrayTwo(arr[i]))
         }else{
             result.push(arr[i])
         }
@@ -29,3 +30,22 @@ const flattenArrayTwo=(arr)=>{
 }
 console.log(flattenArrayOne([1, [2, [3, 4], 5], 6])) // [1,2,3,4,5,6]
 console.log(flattenArrayOne(['a','b',['c','d'],['e',['f','g']],'i'])) // ['a', 'b', 'c','d', 'e', 'f','g', 'i']
+/*
+`concat()` is used because the recursive call returns an array. We want to merge its elements into the result array, not add the whole array as a single item.
+
+    Example:
+
+    let result = [1, 2];
+
+    result.push([3, 4]);
+    console.log(result);
+     Output: [1, 2, [3, 4]]
+
+    result = [1, 2];
+
+    result = result.concat([3, 4]);
+    console.log(result);
+    Output: [1, 2, 3, 4]
+
+    So, `push()` adds the entire array as one element, while `concat()` merges all elements into the existing array, which is why `concat()` is used in recursive array flattening.
+*/
