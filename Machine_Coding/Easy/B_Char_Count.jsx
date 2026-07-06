f2e12a4154811a39b468c8898eda5fca2ba8af54
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-07-06 23:00:13 +0530

    text count and word count

--- Machine_Coding/Easy/B_Char_Count.jsx
+++ Machine_Coding/Easy/B_Char_Count.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
+/*
+->Question:
+    Create a textarea that displays:
+        Total characters
+        Total words
+
+*/
 const B_Char_Count = () => {
-  return (
-    <div>B_Char_Count</div>
-  )
-}
+  const [text, settext] = useState("");
 
-export default B_Char_Count
+  const words=text.trim() ? text.trim().split(/\s+/).length:0;
+  return (
+    <div>
+      <textarea value={text} onChange={(e) => settext(e.target.value)} />
+        <h3>Characters: {text.length} </h3>
+        <h3>Words: {words} </h3>
+    </div>
+  );
+};
+
+export default B_Char_Count;
