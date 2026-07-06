9c458af11364ae6c2a0450d76e6f698fa9884cea
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-07-06 22:55:46 +0530

    Medium questions added

---
+++ Machine_Coding/Easy/B_Char_Count.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const B_Char_Count = () => {
+  return (
+    <div>B_Char_Count</div>
+  )
+}
+
+export default B_Char_Count
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import A_Debounce from "../Machine_Coding/A_useDebounce_Custom"
+import A_Debounce from "../Machine_Coding/Medium/A_useDebounce_Custom"
 import A_Counter from "../Machine_Coding/Easy/A_Counter"
+import B_Char_Count from "../Machine_Coding/Easy/B_Char_Count"
 
 function App() {
 
   return (
     <div>
-      {/* Easy Machine Coding Questions: */}
-      <A_Counter/>
+      {/* ************** Easy Machine Coding Questions ***************** */}
+      {/* <A_Counter/> */}
+      <B_Char_Count/>
       
 
 
 
 
-
+       {/* ************ Medium Machine Coding Questions ****************** */}
       {/* <A_Debounce/> */}
     </div>
   )
