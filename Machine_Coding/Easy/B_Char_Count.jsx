5beebbe3bad8ffe9a7244c60c78f3094c16a7c97
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-07-07 10:42:03 +0530

    explanation

--- Machine_Coding/Easy/B_Char_Count.jsx
+++ Machine_Coding/Easy/B_Char_Count.jsx
@@ -10,7 +10,7 @@ import React, { useState } from "react";
 const B_Char_Count = () => {
   const [text, settext] = useState("");
 
-  const words=text.trim() ? text.trim().split(/\s+/).length:0;
+  const words=text.trim() ? text.trim().split(/\s+/).length:0; // Explanation below:
   return (
     <div>
       <textarea value={text} onChange={(e) => settext(e.target.value)} />
@@ -19,5 +19,44 @@ const B_Char_Count = () => {
     </div>
   );
 };
+
+/*
+Explanation:
+    ->  const words=text.trim() ? text.trim().split(/\s+/).length:0;
+
+    1. text.trim() : trim() removes whitespace from the beginning and end of the string.
+
+    2. text.trim() ? : "ternary operator." here If the trimmed string is not empty, it is truthy.
+                                          If it is empty (""), it is falsy.
+                       Example: text = "Hello";
+                                text.trim(); // "Hello" (truthy)
+
+                                text = "     ";
+                                text.trim(); // "" (falsy)
+
+    3. text.trim().split(/\s+/):
+                split() converts the string into an array.
+                The separator here is: /\s+/ "" This is a regular expression.
+
+                                        \s: means any whitespace character, including: Space " ", Tab \t , Newline \n,
+                                        
+                                        + : means one or more.
+                                        
+                                        So, /\s+ means: "Split wherever there are one or more whitespace characters."
+
+                    Example:
+
+                    const text = "Hello    World   React";
+                    console.log(text.split(/\s+/));
+                    Output:
+                    ["Hello", "World", "React"]
+
+                    Notice that even though there are multiple spaces, you still get only three words.
+
+    4. .length :Now we count the array items.
+            ["Hello", "World", "React"].length
+            Output: 3
+
+*/
 
 export default B_Char_Count;
