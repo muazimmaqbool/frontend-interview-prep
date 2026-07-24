7efbe5a9329c97538d035e3aa3fb1fcfa1b6b316
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-07-24 09:02:42 +0530

    styling and code explanation

--- Machine_Coding/Medium/J_Nested_Comments.jsx
+++ Machine_Coding/Medium/J_Nested_Comments.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //Question: Render nested comments recursively.
 
+// Sample nested comments data.
 const comments = [
   {
     id: 1,
@@ -24,8 +25,30 @@ const comments = [
 
 function Comment({ comment }) {
   return (
-    <div style={{ marginLeft: 20 }}>
-      <p>{comment.text}</p>
+    <div
+      style={{
+        marginLeft: "20px", // Indent child comments
+        marginTop: "10px",
+        borderLeft: "2px solid #ccc",
+        paddingLeft: "15px",
+      }}
+    >
+      {/* Display current comment */}
+      <div
+        style={{
+          background: "#f5f5f5",
+          padding: "10px",
+          borderRadius: "6px",
+        }}
+      >
+        {comment.text}
+      </div>
+      {/* 
+        Loop through all replies.
+        If a reply has its own replies,
+        the Comment component calls itself again.
+      */}
+
       {comment.replies.map((reply) => (
         <Comment key={reply.id} comment={reply} />
       ))}
@@ -34,8 +57,15 @@ function Comment({ comment }) {
 }
 const J_Nested_Comments = () => {
   return (
-    <div>
+    <div
+      style={{
+        maxWidth: "500px",
+        margin: "30px auto",
+        fontFamily: "Arial",
+      }}
+    >
       <h2>Nested Comments</h2>
+      {/* Render all top-level comments  */}
       {comments.map((c) => (
         <Comment key={c.id} comment={c} />
       ))}
