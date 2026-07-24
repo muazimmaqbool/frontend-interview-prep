e07a7bf3d0bf85fd37f26d48916b325bd16eb3b7
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-07-24 08:48:20 +0530

    logic of drop exaplained

--- Machine_Coding/Medium/I_Drag_Drop_List.jsx
+++ Machine_Coding/Medium/I_Drag_Drop_List.jsx
@@ -2,14 +2,39 @@ import React, { useState } from "react";
 
 //Question: Reorder a list using drag-and-drop.
 const I_Drag_Drop_List = () => {
-  const [items, setItems] = useState(["React", "Vue", "Angular"]);
+  const [items, setItems] = useState([
+    "React",
+    "Vue",
+    "Angular",
+    "Svelte",
+    "Next.js",
+    "Node.js",
+    "TypeScript",
+    "Tailwind CSS",
+  ]);
 
+  // Stores the index of the item currently being dragged.
+  // Example: Dragging "Angular" => dragIndex = 2
   const [dragIndex, setdragIndex] = useState(null);
 
+  // Runs when an item is dropped.
   const handleDrop = (dropIndex) => {
+    //Create a copy of the items array. Never mutate React state directly.
     const updated = [...items];
-    const [dragged] = updated.splice(dragIndex, 1);
+
+    // Remove the dragged item from its original position.
+    // Example:["React","Vue","Angular","Svelte"]
+    // dragIndex = 2
+    // updated.splice(2,1) => removed item = ["Angular"]
+    // updated becomes: ["React","Vue","Svelte"]
+    const [dragged] = updated.splice(dragIndex, 1); // Array destructuring extracts the removed item inside dragged
+
+    // Insert the dragged item at the dropped position.
+    // Example: dropIndex = 0
+    // Result: ["Angular","React","Vue","Svelte"]
     updated.splice(dropIndex, 0, dragged);
+
+    // Update state so React re-renders.
     setItems(updated);
   };
   return (
@@ -21,7 +46,7 @@ const I_Drag_Drop_List = () => {
           draggable
           onDragStart={() => setdragIndex(index)}
           onDragOver={(e) => e.preventDefault()}
-          onDrop={()=>handleDrop(index)}
+          onDrop={() => handleDrop(index)}
         >
           {item}
         </div>
