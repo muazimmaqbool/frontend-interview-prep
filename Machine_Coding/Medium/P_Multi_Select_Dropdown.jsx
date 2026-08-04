e8ae2ce769470070de1e551c6e8f3c943dab895e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-04 14:58:25 +0530

    handle multile selected dropdown

--- Machine_Coding/Medium/P_Multi_Select_Dropdown.jsx
+++ Machine_Coding/Medium/P_Multi_Select_Dropdown.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
 
+const options = ["React", "JavaScript", "TypeScript", "Node.js", "MongoDB"];
 const P_Multi_Select_Dropdown = () => {
-  return (
-    <div>
-      <h2>Multi Select Dropdown</h2>
-    </div>
-  )
-}
+  const [selected, setselected] = useState([]);
+  const [isOpen, setisOpen] = useState(false);
 
-export default P_Multi_Select_Dropdown
+  const handleSelect = (option) => {
+    setselected((prev) =>
+      prev.includes(option)
+        ? prev.filter((item) => item !== option)
+        : [...prev, option],
+    );
+  };
+  return (
+    <div style={{ width: "400px", margin: "40px auto", position: "relative" }}>
+      <h2>Multi Select Dropdown</h2>
+      {/* dropdown header */}
+      <div
+        style={{
+          border: "1px solid #ccc",
+          padding: "10px",
+          borderRadius: "6px",
+          cursor: "pointer",
+          background: "#fff",
+        }}
+        onClick={() => setisOpen((prev) => !prev)}
+      >
+        {selected.length ? selected.join(", ") : "Select Technology"}
+      </div>
+      {/* dropdown options */}
+      {isOpen && (
+        <div
+          style={{
+            border: "1px solid #ccc",
+            marginTop: "5px",
+            borderRadius: "6px",
+            background: "#fff",
+          }}
+        >
+          {options.map((option) => (
+            <label
+              key={option}
+              style={{
+                display: "block",
+                padding: "10px",
+                cursor: "pointer",
+              }}
+            >
+              <input
+                type="checkbox"
+                checked={selected.includes(option)}
+                onChange={() => handleSelect(option)}
+              />
+              {option}
+            </label>
+          ))}
+        </div>
+      )}
+      {/* selected Values */}
+      <div style={{ marginTop: "20px" }}>
+        {selected.length ? selected.join(", ") : "None"}
+      </div>
+    </div>
+  );
+};
+
+export default P_Multi_Select_Dropdown;
