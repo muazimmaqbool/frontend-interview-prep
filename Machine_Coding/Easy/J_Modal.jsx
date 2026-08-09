0ccb40c59e0dffda8e9723e822fa315abacebab0
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-09 20:56:03 +0530

    extra

--- Machine_Coding/Easy/J_Modal.jsx
+++ Machine_Coding/Easy/J_Modal.jsx
@@ -9,7 +9,7 @@ const J_Modal = () => {
       <button onClick={() => setopen(true)}>Open Modal</button>
 
       {open && (
-        <div
+        <div //this dev is background of the modal like will be shown to the rest of the screen
           style={{ position: "fixed", inset: 0, background: "rgba(0,0,0,0.5)" }}
         >
           {/* inset: 0; is a shorthand property that sets top: 0;, right: 0;, bottom: 0;, and left: 0; all at once */}
