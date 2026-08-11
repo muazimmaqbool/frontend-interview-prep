87122bab560764ecdc6b6eed098c24e4ef7aed11
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-11 10:23:50 +0530

    watch is currying

--- Javascript_Coding_Questions/Medium/9_Implement_Curry.js
+++ Javascript_Coding_Questions/Medium/9_Implement_Curry.js
@@ -3,6 +3,16 @@ Create a curry function that accepts a function fn and returns a curried version
 should accept arguments one at a time, and once all arguments are received (based on the original function's
 arity), the original function should be executed.
 
+->What is currying?
+    Currying is a technique where a function that normally takes multiple arguments is transformed into a sequence of functions that each take some arguments.
+    For example, instead of:
+        function add(a, b, c) {
+            return a + b + c;
+        }
+        add(1, 2, 3);
+        
+        ->With currying, you can do: add(1)(2)(3);
+
 Example Inputs & Outputs:
         function sum(a, b, c) {
             return a + b + c;
