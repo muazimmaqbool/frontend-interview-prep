f1d05c2aec42b81938b54ea11a9ad3152a8e16da
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-14 20:52:28 +0530

    back to top questoin added

---
+++ Machine_Coding/Easy/L_Back_To_Top.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+/*
+Create a floating Back to Top button that appears when the user scrolls down the page and, when clicked, smoothly scrolls back to the top.
+
+Things to do:
+    . Add a Back to Top button to the page.
+    . Keep it hidden when the scroll position is near the top.
+    . Show it only when the user scrolls beyond a certain distance (e.g., 500px).
+    . When clicked, smoothly scroll the page to the top.
+    . Make sure the button is positioned so it's always visible on the screen when shown.
+
+Note:
+    . Ensure the page has enough content to allow scrolling (e.g., multiple paragraphs or elements) so that the Back to Top button can be tested.
+*/
+const L_Back_To_Top = () => {
+  return (
+    <div>L_Back_To_Top</div>
+  )
+}
+
+export default L_Back_To_Top
