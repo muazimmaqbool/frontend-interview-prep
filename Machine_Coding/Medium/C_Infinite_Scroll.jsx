e874503f106f211e601c5e37c64ab11e84ef0ca9
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-18 22:08:05 +0530

    infinite scroll

--- Machine_Coding/Medium/C_Infinite_Scroll.jsx
+++ Machine_Coding/Medium/C_Infinite_Scroll.jsx
@@ -12,19 +12,18 @@ const allItems = Array.from({ length: 100 }, (_, i) => `Item ${i + 1}`);
 const C_Infinite_Scroll = () => {
   const [count, setcount] = useState(20);
 
-//responsible for detecting when the user reaches the bottom of the page and then loads 20 more items.
+  //responsible for detecting when the user reaches the bottom of the page and then loads 20 more items.
   useEffect(() => {
-
     //this function executes every time the user scrolls
     const handleScroll = () => {
-        //Checks if user has reached the bottom:
+      //Checks if user has reached the bottom:
       if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         //window.innerHeight: Height of the visible browser window. Eg: 800px
         //window.scrollY: How much the page has been scrolled. Eg: Suppose you've scrolled to 1200px
         //Add them together i.e 800 + 1200 = 2000px
 
         //Now:document.body.offsetHeight: Total height of the page. suppose its 2000px
-        // now 2000px >=2000px true means: The bottom of the visible screen has reached the bottom of the page.
+        //now 2000px >=2000px true means: The bottom of the visible screen has reached the bottom of the page.
 
         //Load 20 more items
         setcount((c) => Math.min(c + 20, allItems.length));
@@ -34,7 +33,7 @@ const C_Infinite_Scroll = () => {
         //so Math.min(110,100) it will return 100
       }
     };
-    
+
     //Register event listener: this tells the browser that whenever the page scrolls, execute handleScroll().
     window.addEventListener("scroll", handleScroll);
 
@@ -46,7 +45,9 @@ const C_Infinite_Scroll = () => {
       <h2>Infinite Scroll</h2>
       <h3>Available Items:</h3>
       {allItems.slice(0, count).map((item) => (
-        <p style={{fontSize:"1.5rem"}} key={item}>{item}</p>
+        <p style={{ fontSize: "1.5rem" }} key={item}>
+          {item}
+        </p>
       ))}
     </div>
   );
